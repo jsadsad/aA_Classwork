7b8d1ea7f6caba5393cf98c24dc39a55e126cb14
Joshua Clarence D. Sadsad <joshsad2@gmail.com>
2020-12-07 11:25:34 -0800

    created #valid_move?

--- W4D1/knights_travails.rb
+++ W4D1/knights_travails.rb
@@ -16,8 +16,22 @@ class KnightPathFinder
     #check if index1 == +/- 1 of current position index1 and index2 == +/- 2 of current index2, or vv
     #return array of valid moves
     valid_moves = []
-    
+    (0..7).each do |row|
+      (0..7).each do |col|
+        move = [row, col] #x, y
+        valid_moves << move if valid_move?(move)
+      end
+    end
     valid_moves
+  end
+
+  def valid_move?(move) # [1,2]
+    if move[0] == (@starting_pos[0] + 1 || @starting_pos[0] - 1) && move[1] == (@starting_pos[1] + 2 || @starting_pos[1] - 2)
+      return true
+    elsif move[0] == (@starting_pos[0] + 2 || @starting_pos[0] - 2) && move[1] == (@starting_pos[1] + 1 || @starting_pos[1] - 1)
+      return true
+    end
+    false
   end
 end
 
