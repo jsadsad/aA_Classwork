ec34694e2d2357abcc1401f62710c4f7cb974e61
Bogey <jeffreysbogart@gmail.com>
2020-12-07 11:07:41 -0800

    Adds outline for self.valid_moves

--- W4D1/knights_travails.rb
+++ W4D1/knights_travails.rb
@@ -4,14 +4,20 @@ class KnightPathFinder
   def initialize(pos)
     @root_node = PolyTreeNode.new(pos)
     @considered_positions = Array.new
+    @starting_pos = pos
     # build_move_tree
   end
 
   def build_move_tree
   end
 
-  def self.valid_move
-
+  def self.valid_moves
+    #nested iteration 0-7 each
+    #check if index1 == +/- 1 of current position index1 and index2 == +/- 2 of current index2, or vv
+    #return array of valid moves
+    valid_moves = []
+    
+    valid_moves
   end
 end
 
