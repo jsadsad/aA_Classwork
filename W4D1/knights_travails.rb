575e1425b336010c3b554cb58a25f60d654731ed
Bogey <jeffreysbogart@gmail.com>
2020-12-07 11:58:46 -0800

    Adds valid_moves and valid_move? class methods

--- W4D1/knights_travails.rb
+++ W4D1/knights_travails.rb
@@ -1,5 +1,8 @@
+require_relative "polytreenode.rb"
+
 class KnightPathFinder
   VALID_MOVES = []
+  attr_reader :starting_pos
 
   def initialize(pos)
     @root_node = PolyTreeNode.new(pos)
@@ -11,7 +14,18 @@ class KnightPathFinder
   def build_move_tree
   end
 
-  def self.valid_moves
+  def self.valid_move?(move, pos) # move = [0, 4], pos = [1,2]
+    if (move[0] == pos[0] + 1 || move[0] == pos[0] - 1) && 
+      (move[1] == pos[1] + 2 || move[1] == pos[1] - 2)
+      return true
+    elsif (move[0] == pos[0] + 2 || move[0] == pos[0] - 2) && 
+      (move[1] == pos[1] + 1 || move[1] == pos[1] - 1)
+      return true
+    end
+    false
+  end
+
+  def self.valid_moves(pos)
     #nested iteration 0-7 each
     #check if index1 == +/- 1 of current position index1 and index2 == +/- 2 of current index2, or vv
     #return array of valid moves
@@ -19,20 +33,14 @@ class KnightPathFinder
     (0..7).each do |row|
       (0..7).each do |col|
         move = [row, col] #x, y
-        valid_moves << move if valid_move?(move)
+        valid_moves << move if valid_move?(move, pos)
       end
     end
     valid_moves
   end
 
-  def valid_move?(move) # [1,2]
-    if move[0] == (@starting_pos[0] + 1 || @starting_pos[0] - 1) && move[1] == (@starting_pos[1] + 2 || @starting_pos[1] - 2)
-      return true
-    elsif move[0] == (@starting_pos[0] + 2 || @starting_pos[0] - 2) && move[1] == (@starting_pos[1] + 1 || @starting_pos[1] - 1)
-      return true
-    end
-    false
-  end
 end
 
-# kpf = KnightPathFinder.new([0, 0])
+# kpf = KnightPathFinder.new([1, 2])
+# p KnightPathFinder.valid_moves(kpf.starting_pos)
+# p KnightPathFinder.valid_move?([0, 4], [1, 2])
