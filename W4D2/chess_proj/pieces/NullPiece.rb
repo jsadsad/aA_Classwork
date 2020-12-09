d4848c9fd9ec99fb4e7c158ee3772ad9495353cd
Joshua Clarence D. Sadsad <joshsad2@gmail.com>
2020-12-09 11:34:16 -0800

    added #forward_steps to Pawn class

--- W4D2/chess_proj/pieces/NullPiece.rb
+++ W4D2/chess_proj/pieces/NullPiece.rb
@@ -2,8 +2,14 @@ require 'singleton'
 require_relative 'piece'
 
 class NullPiece < Piece 
+    attr_reader :symbol
     include Singleton
 
     def initialize
+        @symbol = " "
+    end
+
+    def moves
+        []
     end
 end
--- W4D2/chess_proj/pieces/pawn.rb
+++ W4D2/chess_proj/pieces/pawn.rb
@@ -1,2 +1,43 @@
-class Pawn
+require_relative 'piece'
+
+class Pawn < Piece
+  def symbol
+  end
+
+  def moves
+    forward_steps + side_attacks
+  end
+
+  private
+
+  def forward_steps
+    i, j = pos
+    one_step = [i + forward_dir, j]
+    two_step = [i + (2*forward_dir), j]
+    return [] unless board.valid_move?(one_step)
+    moves = [one_step]
+    moves << two_step if at_start_row? # add empty?
+    moves
+    # incrementing i
+    #if we can't move 1 step, we can't move 2
+    # 1 step = [i + forward_dir,j] # [6,1] => [5,1]
+    # 2 step = [i + 2*forward_dir,j] #[6,1] => [4,1]
+    # if we can't move 1 step, moves should return []
+    # else moves[1 step]
+    # << 2 step if at_start_row? == true && pos.empty?
+    # return moves
+  end
+
+  def side_attacks
+    
+  end
+
+  def at_start_row?
+    color == "white" ? pos[0] == 6 : pos[0] == 1
+  end
+
+  def forward_dir
+    color == "white" ? -1 : 1 
+  end
+
 end
