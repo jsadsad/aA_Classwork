7dba321b4d6fcc252bc50d9ff0399019b1bf427c
Janette <tinoconjanette@gmail.com>
2020-12-11 13:35:12 -0800

    Add Phase1 of largests sum

--- W4D5/execution_time_diff.rb
+++ W4D5/execution_time_diff.rb
@@ -2,26 +2,26 @@
 
 #phase 1
 # compares each element to every other element of the list. Return the element if all other elements in the array are larger.
-# def my_min(list)
-#   min = 0
-#   list.each do |ele|
-#     list.each do |ele2|
-#       min = ele if ele < ele2 && ele < min
-#     end
-#   end
-#   min
-# end
+def my_min(list)
+  min = 0
+  list.each do |ele|
+    list.each do |ele2|
+      min = ele if ele < ele2 && ele < min
+    end
+  end
+  min
+end
 
 #O(n^2) because we are iterating through the array 2 times for each element
 
 #phase 2
-# def my_min(list)
-#   min = list[0]
-#   (0...list.length).each do |i| 
-#     min = list[i] if list[i] < min
-#   end
-#   min
-# end
+def my_min(list)
+  min = list[0]
+  (0...list.length).each do |i| 
+    min = list[i] if list[i] < min
+  end
+  min
+end
 
 #O(n) because we are iterating through the array only once.
 
@@ -32,9 +32,16 @@
 
 def largest_contiguous_subsum(list)
   subs = []
+  list.each_with_index do |ele1, idx1|
+    (idx1..list.length-1).each do |idx2|
+      subs.push(list[idx1..idx2])
+    end
+  end
+  subs_sum = subs.map{|sub_arr| sub_arr.sum}
+  subs_sum.flatten.max
 end
 # list = [5, 3, -7]
-# largest_contiguous_subsum(list) # => 8
+# p largest_contiguous_subsum(list) # => 8
 
 # possible sub-sums
 # [5]           # => 5
@@ -43,3 +50,24 @@ end
 # [3]           # => 3
 # [3, -7]       # => -4
 # [-7]          # => -7
+
+# list = [2, 3, -6, 7, -6, 7]
+[2],[2,3], [2,3,-6],..., [3], [3 -6]
+
+# p largest_contiguous_subsum(list)
+
+# list = [-5, -1, -3]
+# p largest_contiguous_subsum(list) # => -1 (from [-1])
+
+##O(n^2 + n)
+#O(n^3)
+
+def largest_contiguous_subsum(list)
+  largest_sum = 0
+  current_sum = list[0]
+  (1...list.length).each do |i|
+    current_sum += list[i]
+    largest_sum = current_sum if current_sum > largest_sum
+
+  end
+end
