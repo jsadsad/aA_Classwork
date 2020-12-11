22e6c72da4db482a9a59c7b03ad075d36de42cc0
droid4alex <droid4alex@gmail.com>
2020-12-10 16:33:20 -0800

    Worked on TDD classwork 15min

--- W4D4/first_tdd_proj/lib/towers_of_hanoi.rb
+++ W4D4/first_tdd_proj/lib/towers_of_hanoi.rb
@@ -2,9 +2,10 @@ class TowersOfHanoi
   attr_reader :game_over
 
   def initialize
-    @towers = [[1,2,3,4], [], []]
+    @towers = [[1,2,3], [], []]
     @game_over = false
-    @towers_win = [[], [], [1,2,3,4]]
+    @towers_win = [[], [], [1,2,3]]
+    @turn = 1
     play
   end
 
@@ -12,10 +13,33 @@ class TowersOfHanoi
     until won?
       move
     end
-    game_over
+    p 'YOU WON!!!'
+    p @towers
+  end
+
+  def won?    
+    # game is won if board[0] == dupped_board[-1]
+    # board - [1,2,3,4] == duped-board [1,2,3,4]
+    @game_over = true
+    @towers == @towers_win
   end
 
   def move
+    p @towers
+    if @turn != 1
+      puts 'What number do you want to move?'
+      pick_number = gets.chomp.to_i
+      puts 'What tower do you want to put on?'
+      pick_tower = gets.chomp.to_i
+      firstnum = @towers[pick_number].shift
+      @towers[pick_tower].unshift(firstnum)
+    else
+      puts 'Where do you want to put the first disc number?'
+      pick_tower = gets.chomp.to_i
+      firstnum = @towers[0].shift
+      @towers[pick_tower].unshift(firstnum)
+    end
+    
     # user can only move the first value of an sub array
     # => [ [1,2,3,4], [], [] ] = board
     # => [ [2,3,4], [1], [] ] shift(1) and then unshifted to 2nd array
@@ -23,11 +47,8 @@ class TowersOfHanoi
     # => [ [4], [1], [3,2] ] 
     # shift removes first ele
     # unshift adds to front of array
-  end
-
-  def won?
-    @towers == @towers_win
-    # game is won if board[0] == dupped_board[-1]
-    # board - [1,2,3,4] == duped-board [1,2,3,4]
+    @turn += 1
   end
 end
+
+game = TowersOfHanoi.new
