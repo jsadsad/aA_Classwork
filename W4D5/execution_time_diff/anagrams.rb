29586238a4af52584462fceaa448d25a3e56baff
Janette <tinoconjanette@gmail.com>
2020-12-11 14:29:37 -0800

    Add Anagrams#phase2

--- W4D5/execution_time_diff/anagrams.rb
+++ W4D5/execution_time_diff/anagrams.rb
@@ -20,3 +20,23 @@ end
 
 # O(n!) because permutations. Recursive loop on the length of the string - 1
 
+def second_anagram?(word, anagram)
+  #debugger
+  #iterating through anagram and if we find that letter inisde word, then delete it from the anagram
+  split_word = word.split("")
+  split_anagram = anagram.split("")
+  split_word.each do |char|
+    if split_anagram.include?(char)
+      target_idx = split_anagram.find_index(char)
+      split_anagram.delete_at(target_idx)
+    end
+  end
+  split_anagram.empty?
+end
+
+#O(n^4) because of .each . include .find _index .delete_at (4 nested loops)
+
+p second_anagram?("gizmo", "sally")    #=> false
+p second_anagram?("elvis", "lives")    #=> true
+
+
