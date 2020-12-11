fb8f08f0309d788f4cb835a43909843eade4fddb
Joshua Clarence D. Sadsad <joshsad2@gmail.com>
2020-12-11 15:20:02 -0800

    completed anagrams

--- W4D5/execution_time_diff/anagrams.rb
+++ W4D5/execution_time_diff/anagrams.rb
@@ -3,7 +3,6 @@ require 'byebug'
 # Write a method #first_anagram? that will generate and store all the possible anagrams of the first string. Check if the second string is one of these.
 
 # Hints:
-
 def anagrams?(word, anagram)
   # subs = []
   # (0...word.length).each do |i1|
@@ -25,18 +24,59 @@ def second_anagram?(word, anagram)
   #iterating through anagram and if we find that letter inisde word, then delete it from the anagram
   split_word = word.split("")
   split_anagram = anagram.split("")
-  split_word.each do |char|
-    if split_anagram.include?(char)
-      target_idx = split_anagram.find_index(char)
-      split_anagram.delete_at(target_idx)
+  split_word.each do |char| # 1
+    if split_anagram.include?(char) 
+      target_idx = split_anagram.find_index(char) # not nested. 
+      split_anagram.delete_at(target_idx) # not nested.
     end
   end
   split_anagram.empty?
 end
 
-#O(n^4) because of .each . include .find _index .delete_at (4 nested loops)
+#O(n * m) - we don't know the size of the strings. Polynomial 
 
-p second_anagram?("gizmo", "sally")    #=> false
-p second_anagram?("elvis", "lives")    #=> true
+# p second_anagram?("gizmo", "sally")    #=> false
+# p second_anagram?("elvis", "lives")    #=> true
+
+# Write a method #third_anagram? that solves the problem by sorting both strings alphabetically. The strings are then anagrams if and only if the sorted versions are the identical.
+
+# What is the time complexity of this solution? Is it better or worse than #second_anagram??
+
+def third_anagram?(word, anagram)
+  word_arr = word.split("").sort!
+  anagram_arr = anagram.split("").sort!
+
+  return word_arr == anagram_arr
+end
+
+# m is to define the other variable.
+# (n log n) + (m log m) + m + n
+# .sort is a `(n log n) because it is a quicksort`
+# .split is an O(n)
+
+# def fourth_anagram?(word, anagram)
+#   word_hash = Hash.new(0)
+#   anagram_hash = Hash.new(0)
+
+#   word.each_char { |char| word_hash[char] += 1 }
+#   anagram.each_char { |char| anagram_hash[char] += 1 }
+
+#   word_hash == anagram_hash
+# end
+
+def fourth_anagram?(word, anagram)
+  word_hash = Hash.new(0)
+
+  word.each_char { |char| word_hash[char] += 1 }
+  anagram.each_char { |char| word_hash[char] -= 1 }
+
+  word_hash.all? { |k,v| v == 0 }
+end
+
+# O(n) + m
+# mention m when the lengths are different
+
+p fourth_anagram?("gizmo", "sally")    #=> false
+p fourth_anagram?("elvis", "lives")    #=> true
 
 
