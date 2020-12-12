ec301bc8f1694d44bc64455106fc0c535ea4e7bc
Joshua Clarence D. Sadsad <joshsad2@gmail.com>
2020-12-11 16:33:22 -0800

    finished #okay_two_sum

--- W4D5/execution_time_diff/two_sum.rb
+++ W4D5/execution_time_diff/two_sum.rb
@@ -10,14 +10,27 @@ def bad_two_sum?(arr, target)
 end
 
 ##O(n^2) because we have 2 nested loops iterating twice
-##we want to have a value for arr1 and arr0, so that 
-def okay_two_sum?(arr, target)
-    sorted = arr.sort
 
+##we want to have a value for arr[0] and arr[1]
+def okay_two_sum?(arr, target)
+    sorted = arr.sort # (n log n)
+    i = 0
+    j = arr.length - 1
+    while i < j # n
+        case arr[i] + arr[j] <=> target # 1
+        when 1
+            j -= 1
+        when 0
+            return true
+        when -1
+            i += 1
+        end
+    end
+    false
 end
 
-
+# O(n log n) + n because sort is (n log n). O(n log n) dominates the space/time.
 
 arr = [0, 1, 5, 7]
-p bad_two_sum?(arr, 6) # => should be true
-p bad_two_sum?(arr, 10) # => should be false
+p okay_two_sum?(arr, 6) # => should be true
+p okay_two_sum?(arr, 10) # => should be false
