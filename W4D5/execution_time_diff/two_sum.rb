8cb45fc67484cd3d298bf9b8fcdcbd0d02e3f1ed
Janette <tinoconjanette@gmail.com>
2020-12-11 17:30:31 -0800

    Add Two_sum hash problem

--- W4D5/execution_time_diff/two_sum.rb
+++ W4D5/execution_time_diff/two_sum.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 def bad_two_sum?(arr, target)
     arr.each_with_index do |ele1, idx1|
         arr.each_with_index do |ele2, idx2|
@@ -31,6 +32,33 @@ end
 
 # O(n log n) + n because sort is (n log n). O(n log n) dominates the space/time.
 
+# arr = [0, 1, 5, 7]
+# p okay_two_sum?(arr, 6) # => should be true
+# p okay_two_sum?(arr, 10) # => should be false
+
+
+def hash_two_sum?(arr, target)
+    #debugger
+    hash_sum = Hash.new
+    arr.each do |ele|
+        hash_sum[ele] = true
+    end
+    arr.each do |ele|
+        ans = target - ele
+        return true if hash_sum[ans]
+    end
+    false
+end
+#{0:true , 1:true , 5:true , 7 :true
 arr = [0, 1, 5, 7]
-p okay_two_sum?(arr, 6) # => should be true
-p okay_two_sum?(arr, 10) # => should be false
+p hash_two_sum?(arr, 6) # => should be true
+p hash_two_sum?(arr, 10) # => should be false
+## hash.any?{|k,v|}
+#variable = hash[]
+#
+
+##iterate through the array one elemt at a time and check 
+#ans = target - ele 
+#return true if hash[ans]
+
+##false
