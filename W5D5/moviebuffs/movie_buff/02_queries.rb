2157eeb47994091d5ca06928b67969c06cc415a6
bcdguz <bcdguz96@gmail.com>
2020-12-18 15:04:25 -0800

    Working on 02_queries in movie_buff

--- W5D5/moviebuffs/movie_buff/02_queries.rb
+++ W5D5/moviebuffs/movie_buff/02_queries.rb
@@ -11,15 +11,21 @@ end
 def bad_years
   # List the years in which a movie with a rating above 8 was not released.
   Movie
-    .select(:yr)
-    .where.not("yr NOT IN (?)", Movie.select(:yr).where("score > 8").pluck(:yr)).pluck(:yr)
-    
+    .group(:yr)
+    .having("Max(score) < 8")
+    .pluck(:yr)
+
+    # .where.not("yr NOT IN (?)", Movie.select(:yr).where("score > 8").pluck(:yr)).pluck(:yr)
 end
 
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
-
+  Movie
+    .joins(:actors)
+    .select('actors.id, actors.name')
+    .where('movies.title = (?)', title)
+    .order('castings.ord')
 end
 
 def vanity_projects
@@ -28,7 +34,11 @@ def vanity_projects
   # Show the movie id and title and director's name.
 
   # Note: Directors appear in the 'actors' table.
-
+  Movie
+    .joins(:actors)
+    .select('movies.id, movies.title, actors.name')
+    .where("director_id = actors.id")
+    .where("castings.ord = 1")
 end
 
 def most_supportive
