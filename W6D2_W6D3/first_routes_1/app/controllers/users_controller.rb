13ba8ea7a8f5a85c344a380aa20efe1238908fb8
Brandon Estano <estanob@gmail.com>
2020-12-29 15:30:10 -0800

    Beging Users migration

--- W6D2_W6D3/first_routes_1/app/controllers/users_controller.rb
+++ W6D2_W6D3/first_routes_1/app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
   def index
-    render plain: "I'm in the Index action!"
+    render json: params
   end
 
   def create
+    render json: params
+  end
+
+  def show
     render json: params
   end
 end
