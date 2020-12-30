038519ebb23e6da50dfaf8396bc9b8679b3a6ee4
Brandon Estano <estanob@gmail.com>
2020-12-30 15:40:55 -0800

    Complete Phase II

--- W6D2_W6D3/artshare/app/controllers/artworks_controller.rb
+++ W6D2_W6D3/artshare/app/controllers/artworks_controller.rb
@@ -1,41 +1,44 @@
 class ArtworksController < ApplicationController
+
+  # resources :artworks, only: [:create, :destroy, :index, :show, :update]
+
   def index
-    users = User.all
-    render json: users
+    artworks = Artwork.all
+    render json: artworks
   end
 
   def show
-    user = User.find(params[:id])
-    render json: user
+    artwork = Artwork.find(params[:id])
+    render json: artwork
   end
 
   def destroy
-    user = User.find(params[:id])
-    user.destroy
-    render json: user
+    artwork = Artwork.find(params[:id])
+    artwork.destroy
+    render json: artwork
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      render json: user
+    artwork = Artwork.new(artwork_params)
+    if artwork.save
+      render json: artwork
     else
-      render json: user.errors.full_messages, status: :unprocessable_entity
+      render json: artwork.errors.full_messages, status: :unprocessable_entity
     end
   end
 
   def update
-    user = User.find(params[:id])
-    if user.update_attributes(user_params)
-      render json: user
+    artwork = Artwork.find(params[:id])
+    if artwork.update_attributes(artwork_params)
+      render json: artwork
     else
-      render json: user.errors, status: :unprocessable_entity
+      render json: artwork.errors, status: :unprocessable_entity
     end
   end
 
   private
 
-  def user_params
-    params.require(:user).permit(:username)
+  def artwork_params
+    params.require(:artwork).permit(:title, :image_url, :artist_id)
   end
 end
