e3a5bde20eace1438a7391fc2c48a0fffdcac8d7
JCS <joshsad2@gmail.com>
2020-12-29 16:17:01 -0800

    created User model

--- W6D2_W6D3/first_routes_1/db/migrate/20201229232930_create_users.rb
+++ W6D2_W6D3/first_routes_1/db/migrate/20201229232930_create_users.rb
@@ -1,6 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.string :name, presence: true
+      t.string :email, presence: true
     end
   end
 end
