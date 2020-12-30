68635c8bd7583f224acf90d71b863655c654af49
Brandon Estano <estanob@gmail.com>
2020-12-29 16:38:36 -0800

    Create user through API

--- W6D2_W6D3/first_routes_1/app/controllers/users_controller.rb
+++ W6D2_W6D3/first_routes_1/app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
   def index
-    render json: params
+    users = User.all
+    render json: users
   end
 
   def create
-    render json: params
+  user = User.new(params.require(:user).permit(:name, :email))
+  if user.save
+    render json: user
+  else
+    render json: user.errors.full_messages, status: :unprocessable_entity
   end
+end
 
   def show
     render json: params
