7bb9fe96a4598c70d0fef56039da99a82df3478b
JCS <joshsad2@gmail.com>
2020-12-29 18:59:58 -0800

    completed users controller

--- W6D2_W6D3/first_routes_1/app/controllers/users_controller.rb
+++ W6D2_W6D3/first_routes_1/app/controllers/users_controller.rb
@@ -4,21 +4,48 @@ class UsersController < ApplicationController
     render json: users
   end
 
+  # find vs find_by
+  # find_by is used as a helper when you're searching for information within a column, and it maps to such with naming conventions. For instance, if you have a column named name in your database
+  # The find method is usually used to retrieve a row by ID.
+
+  # def show
+  #   user = User.find_by(id: params[:id])
+  #   render json: user
+  # end
+
   def show
     user = User.find(params[:id])
     render json: user
   end
 
-  def create
-  user = User.new(params.require(:user).permit(:name, :email))
-  if user.save
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
     render json: user
-  else
-    render json: user.errors.full_messages, status: :unprocessable_entity
   end
-end
 
-  def show
-    render json: params
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
   end
+
+  def update
+    user = User.find(params[:id])
+    if user.update_attributes(user_params)
+      render json: user
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
 end
