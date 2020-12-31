446e76a49f56a05ffc5fc3626f63c80375928dfe
JCS <joshsad2@gmail.com>
2020-12-31 15:19:57 -0800

    working on form partial

--- W6D5/cats_99/app/controllers/cats_controller.rb
+++ W6D5/cats_99/app/controllers/cats_controller.rb
@@ -1,26 +1,32 @@
 class CatsController < ApplicationController
 
-  def index 
-    cats = Cat.all 
-    render json: cats 
+  def new
+    @cat = Cat.new
+    render :new
+  end
+  
+  def index
+    @cats = Cat.all
+    render :index
   end
 
   def show
-    cat = Cat.find_by(id: params[:id])
+    @cat = Cat.find_by(id: params[:id])
+    render :show
   end
 
   def create
-    cat = Cat.new(cat_params)
-
-    if cat.save 
-      render json: cat 
-    else 
-      render json: cat.errors.full_messages, status: :unprocessable_entity
+    @cat = Cat.new(cat_params)
+    if @cat.save
+      redirect_to cat_url(@cat)
+    else
+      # render json: cat.errors.full_messages, status: :unprocessable_entity
+      render :new
     end
   end
 
-  private 
-    def cat_params 
+  private
+    def cat_params
       params.require(:cat).permit(:name, :birth_date, :color, :sex, :description)
     end
 end
