4180bf39458bf4685467abeaf21313fd6521b1f2
AlexeyS <xengawe@gmail.com>
2020-12-31 17:01:46 -0800

    fix index/show pages

--- W6D5/cats_99/app/controllers/cats_controller.rb
+++ W6D5/cats_99/app/controllers/cats_controller.rb
@@ -25,6 +25,11 @@ class CatsController < ApplicationController
     end
   end
 
+  def edit
+    @cat = Cat.find(params[:id])
+    render :edit    
+  end
+
   private
     def cat_params
       params.require(:cat).permit(:name, :birth_date, :color, :sex, :description)
