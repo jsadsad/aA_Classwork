2df15b3bff2d101537a5baf8ae23201733ebe602
timjkim210 <timjkim210@gmail.com>
2021-01-05 11:16:57 -0800

    tested user model methods

--- W7D2/music-app/app/models/user.rb
+++ W7D2/music-app/app/models/user.rb
@@ -7,22 +7,34 @@ class User < ApplicationRecord
   # 4. reset_session_token
   # 5. ensure_session_token
 
+  after_initialize :generate_session_token
+
+  attr_reader :password
+
   def generate_session_token
-    session_token ||= SecureRandom::url_base64
+    self.session_token ||= SecureRandom::urlsafe_base64
   end
 
   def reset_session_token
-    self.session_token = SecureRandom::url_base64
+    self.session_token = SecureRandom::urlsafe_base64
     self.save! #fail loud
     self.session_token
   end
 
   def password=(password)
     @password = password # validates the password given.
-    password_digest = BCrypt::Password.create(password)
+    self.password_digest = BCrypt::Password.create(password)
   end
 
   def is_password?(password)
     BCrypt::Password.new(self.password_digest).is_password?(password)
   end
+
+  def self.find_by_credentials(email, password)
+    email = User.find_by(email: email)
+
+    email && email.is_password?(password) ? email : nil
+  end
+
+
 end
