8a770f5028e7d367fbc23d930ee018a5fc676e93
timjkim210 <timjkim210@gmail.com>
2021-01-05 14:03:54 -0800

    a lot of testing

--- W7D2/music-app/app/controllers/application_controller.rb
+++ W7D2/music-app/app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
 
+  skip_before_action :verify_authenticity_token
+
   helper_method :current_user, :logged_in?
+
   def login!(user)
-    session[:session_token] = user.reset_session_token!
+    session[:session_token] = user.reset_session_token
   end
 
   def current_user
--- W7D2/music-app/app/controllers/users_controller.rb
+++ W7D2/music-app/app/controllers/users_controller.rb
@@ -1,15 +1,24 @@
 class UsersController < ApplicationController
+
   def new
     @user = User.new
     render :new
   end
 
   def create
-    @user = User.create(user_params)
+    @user = User.new(user_params)
     if @user.save!
       login!(@user)
       redirect_to '/'
-      # bands index
     end
+  end
+
+  def show
+
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
