ca6b26a2a5e09e8c196cf63cc4f11cb4477c9569
JCS <joshsad2@gmail.com>
2021-01-06 17:00:41 -0800

    working on users controller specs

--- W7D3/goal-app/app/controllers/application_controller.rb
+++ W7D3/goal-app/app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  helper_method :require_no_current_user!
+
+  def login!(user)
+    @current_user = user
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def require_no_current_user!
+    redirect_to root_url if current_user
+  end
 end
