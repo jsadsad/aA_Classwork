76275f2fe4ba7526ec44891eb3ff6d5c1ee0df1e
JCS <joshsad2@gmail.com>
2021-01-08 11:25:08 -0800

    added W7D5

---
+++ W7D5/pairboarding.rb
@@ -0,0 +1,29 @@
+
+# You are climbing a stair case. It takes n steps to reach to the top.
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+# Note: Given n will be a positive integer.
+# Example 1:
+# Input: 2
+# Output: 2
+# Explanation: There are two ways to climb to the top.
+# 1. 1 step + 1 step
+# 2. 2 steps
+# Example 2:
+# Input: 3
+# Output: 3
+# Explanation: There are three ways to climb to the top.
+# 1. 1 step + 1 step + 1 step
+# 2. 1 step + 2 steps
+# 3. 2 steps + 1 step
+# 2)
+
+  def climbing_stairs(input)
+  end
+
+# Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+# Example:
+# Input: [0,1,0,3,12]
+# Output: [1,3,12,0,0]
+
+  def zero_sort(array)
+  end
