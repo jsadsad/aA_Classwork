07dc4aa46bdcb4e4b7ce2dbf18550b3cd67e0d67
JCS <joshsad2@gmail.com>
2021-01-11 09:33:48 -0800

    added W8D1

---
+++ W8D1/reddit-rails/app/controllers/application_controller.rb
@@ -0,0 +1,28 @@
+class ApplicationController < ActionController::Base
+  #CRLLL
+
+  helper_method :logged_in?
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def require_logged_in
+    redirect_to new_session_url unless logged_in?
+  end
+
+  def login(user)
+    @current_user = user
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def logout
+    current_user.reset_session_token! if logged_in?
+    @current_user, session[:session_token] = nil
+  end
+
+end
