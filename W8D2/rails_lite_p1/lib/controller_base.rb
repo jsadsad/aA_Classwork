5688876208ba3c7ec33410786714174589d5d441
JCS <joshsad2@gmail.com>
2021-01-12 16:54:49 -0800

    finished phase 2

--- W8D2/rails_lite_p1/lib/controller_base.rb
+++ W8D2/rails_lite_p1/lib/controller_base.rb
@@ -7,21 +7,38 @@ class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
-  def initialize(req, res)
+  def initialize(req, res) #should take a Rack::Request and Rack::Response object as inputs and save them as instance variables (ivars) for later use.
+    @req, @res = req, res
+    @already_built_response ||= false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
-  def redirect_to(url)
+  def redirect_to(url) # Issuing a redirect consists of two parts, setting the 'Location' field of the response header to the redirect url and setting the response status code to 302.
+    @res.status = 302
+    @res["Location"] = url
+    if already_built_response?
+      raise "Double Render!"
+    end
+    @already_built_response = true
+    nil
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    @res.write(content)
+    @res['Content-Type'] = content_type
+    if already_built_response?
+      raise "Double Render!"
+    end
+    @already_built_response = true
+    nil
   end
 
   # use ERB and binding to evaluate templates
@@ -37,4 +54,3 @@ class ControllerBase
   def invoke_action(name)
   end
 end
-
