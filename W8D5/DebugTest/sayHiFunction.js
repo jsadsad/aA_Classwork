a080777e061bf5344f3cdfd30a7b2c1b9fc502fe
JCS <joshsad2@gmail.com>
2021-01-15 09:37:43 -0800

    added W8D5

---
+++ W8D5/DebugTest/sayHiFunction.js
@@ -0,0 +1,22 @@
+function sayHi(name) {
+  debugger
+  console.log(`Say Hi ${name}`)
+}
+
+sayHi("Erik")
+
+
+// DEFAULT settings
+// {
+//     "window.zoomLevel": 2,
+//     "editor.minimap.enabled": true,
+//     "explorer.confirmDragAndDrop": false,
+//     "terminal.integrated.shell.osx": "/bin/bash",
+//     "files.autoSave": "afterDelay",
+//     "editor.tabSize": 2,
+//     "launch": {
+
+//         "configurations": [],
+//         "compounds": []
+//     }
+// }
---
+++ W8D5/notes.md
@@ -0,0 +1,12 @@
+# JavaScript Part 2
+
+# ES6 Features
+
+- Property Declaration: `const obj = {x, y}`
+- Functions: `const foo = (arg1, arg2) => {}`
+
+# Linters
+
+- Find errors _before_ you run your code.
+- Teach and enforce best practices.
+- Enforce style conventions
