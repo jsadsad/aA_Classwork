835f47e2fbca005be4d9771ed103d0db2d119da8
JCS <joshsad2@gmail.com>
2021-01-20 14:27:25 -0800

    worknig on setupboard

--- W9D3/tictactoe_jQuery/src/ttt-view.js
+++ W9D3/tictactoe_jQuery/src/ttt-view.js
@@ -1,17 +1,21 @@
 class View {
-  constructor(game, $el) {}
+  constructor(game, $el) {
+    this.game = game;
+    this.container = $el;
+  }
 
   bindEvents() {}
 
   makeMove($square) {}
 
   setupBoard() {
-    const $newUl = $("<ul></ul>");
-    for (let row=0; row<3; row++){
-      for (let col = 0; col < 3; col++){
-        
+    const $ul = $("<ul>");
+    for (let row = 0; row < 3; row++) {
+      for (let col = 0; col < 3; col++) {
+        let $li = $("<li>")
+        $li.data("pos", [row, col])
+        $ul.append($("<li>Apend</li>"));
       }
-        
     }
   }
 }
