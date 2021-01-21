98301ef9606cbdad51ab0e68f2d62831f5fc9bbe
kinda-dev <maybe.fabio@gmail.com>
2021-01-21 15:27:05 -0800

    work on ajax_twitter project

--- W9D4/AJAX_twitter/frontend/follow_toggle.js
+++ W9D4/AJAX_twitter/frontend/follow_toggle.js
@@ -3,9 +3,25 @@ class FollowToggle {
         this.$el = $el //saves as instance 
         this.userId = $el.data("user-id");
         this.followState = $el.data("follow-state");
+        this.render()
     }
 
+
     // events when user clicks on follow or unfollow go here
+    onClick() {
+        const $el = $('.follow-toggle')
+        $el.on('click', (e) => {
+            if(this.followState === 'followed'){
+                this.followState = 'unfollowed';
+            } else {
+                this.followState = 'followed'
+            }
+        });
+    }
+
+    render() {
+        debugger
+    }
     
 }
 
--- W9D4/AJAX_twitter/frontend/twitter.js
+++ W9D4/AJAX_twitter/frontend/twitter.js
@@ -1,1 +1,11 @@
- 
+ const FollowToggle = require ("./follow_toggle");
+
+ $(() => {
+
+    const $toggleButton = $('button.follow-toggle');
+    
+    $toggleButton.each((idx, htmlEle) => {
+        new FollowToggle($(htmlEle));
+    });
+    
+  });
