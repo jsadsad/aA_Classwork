0d7c17aaba6fb6a86c34aa8b6205754f8ef8ea34
kinda-dev <maybe.fabio@gmail.com>
2021-01-21 17:01:39 -0800

    complete FollowToggle class on AJAX_twitter project

--- W9D4/AJAX_twitter/app/assets/javascripts/bundle.js
+++ W9D4/AJAX_twitter/app/assets/javascripts/bundle.js
@@ -1,11 +1,97 @@
-/******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./frontend/twitter.js");
+/******/ })
+/************************************************************************/
+/******/ ({
 
 /***/ "./frontend/follow_toggle.js":
 /*!***********************************!*\
   !*** ./frontend/follow_toggle.js ***!
   \***********************************/
-/***/ ((module) => {
+/*! no static exports found */
+/***/ (function(module, exports) {
 
 class FollowToggle {
     constructor($el) {
@@ -13,6 +99,7 @@ class FollowToggle {
         this.userId = $el.data("user-id");
         this.followState = $el.data("follow-state");
         this.render()
+        this.handleClick()
     }
 
     // events when user clicks on follow or unfollow go here
@@ -22,23 +109,26 @@ class FollowToggle {
     // else, it should make a DELETE request.
     // On success of the POST / DELETE, we should toggle the followState and re - render.
 
-        const $el = $('button.follow-toggle')
-
-        $el.on('click', (e) => {
+    
+        this.$el.on('click', (e) => {
+            // debugger
             e.preventDefault()
-            if(e.followState === 'followed'){
-                e.followState = 'unfollowed';
-                $ajax({
+            if(this.followState === 'followed'){
+                this.followState = 'unfollowed';
+                this.render()
+                return $.ajax({
                     method: "DELETE",
-                    url: `/user_follow/${this.userId}`,
-                    dataType: "json"
+                    url: `/users/${this.userId}/follow`,
+                    dataType: "JSON"
                 })
             } else {
-                e.followState = 'followed'
-                $ajax({
+                this.followState = 'followed'
+                this.render()
+                // debugger
+                return $.ajax({
                     method: "POST",
-                    url: `/user_follow/${this.userId}`,
-                    dataType: "json"
+                    url: `/users/${this.userId}/follow`,
+                    dataType: "JSON"
                 })
             }
         });
@@ -47,7 +137,7 @@ class FollowToggle {
     render() {
         //where we will display "Follow" or "Unfollow" depending if user follows already
         const $el = $('button.follow-toggle')
-        if ($el.followState === 'followed') {
+        if (this.followState === 'followed') {
             $el.text("Unfollow")
         } else {
             $el.text("Follow")
@@ -57,38 +147,15 @@ class FollowToggle {
 
 module.exports = FollowToggle;
 
-/***/ })
+/***/ }),
 
-/******/ 	});
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		if(__webpack_module_cache__[moduleId]) {
-/******/ 			return __webpack_module_cache__[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-(() => {
+/***/ "./frontend/twitter.js":
 /*!*****************************!*\
   !*** ./frontend/twitter.js ***!
   \*****************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
  const FollowToggle = __webpack_require__ (/*! ./follow_toggle */ "./frontend/follow_toggle.js");
 
  $(() => {
@@ -101,8 +168,8 @@ module.exports = FollowToggle;
     
   });
 
-})();
 
-/******/ })()
-;
+/***/ })
+
+/******/ });
 //# sourceMappingURL=bundle.js.map
--- W9D4/AJAX_twitter/frontend/follow_toggle.js
+++ W9D4/AJAX_twitter/frontend/follow_toggle.js
@@ -4,6 +4,7 @@ class FollowToggle {
         this.userId = $el.data("user-id");
         this.followState = $el.data("follow-state");
         this.render()
+        this.handleClick()
     }
 
     // events when user clicks on follow or unfollow go here
@@ -13,23 +14,26 @@ class FollowToggle {
     // else, it should make a DELETE request.
     // On success of the POST / DELETE, we should toggle the followState and re - render.
 
-        const $el = $('button.follow-toggle')
-
-        $el.on('click', (e) => {
+    
+        this.$el.on('click', (e) => {
+            // debugger
             e.preventDefault()
-            if(e.followState === 'followed'){
-                e.followState = 'unfollowed';
-                $ajax({
+            if(this.followState === 'followed'){
+                this.followState = 'unfollowed';
+                this.render()
+                return $.ajax({
                     method: "DELETE",
-                    url: `/user_follow/${this.userId}`,
-                    dataType: "json"
+                    url: `/users/${this.userId}/follow`,
+                    dataType: "JSON"
                 })
             } else {
-                e.followState = 'followed'
-                $ajax({
+                this.followState = 'followed'
+                this.render()
+                // debugger
+                return $.ajax({
                     method: "POST",
-                    url: `/user_follow/${this.userId}`,
-                    dataType: "json"
+                    url: `/users/${this.userId}/follow`,
+                    dataType: "JSON"
                 })
             }
         });
@@ -38,7 +42,7 @@ class FollowToggle {
     render() {
         //where we will display "Follow" or "Unfollow" depending if user follows already
         const $el = $('button.follow-toggle')
-        if ($el.followState === 'followed') {
+        if (this.followState === 'followed') {
             $el.text("Unfollow")
         } else {
             $el.text("Follow")
