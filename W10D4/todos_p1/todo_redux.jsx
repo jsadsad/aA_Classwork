1f44b79a9ecde9cd8f6717c3006a8b9a29ed8394
JCS <joshsad2@gmail.com>
2021-01-28 11:48:15 -0800

    Done with phase 1 setup

---
+++ W10D4/todos_p1/todo_redux.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("content");
+    ReactDOM.render(<h1>Todos App</h1>, root )
+})
