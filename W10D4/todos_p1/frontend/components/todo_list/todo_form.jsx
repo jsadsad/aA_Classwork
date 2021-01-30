76879e399875d778ce01ba8d01b2bdac0fb81674
Michelle Ha <michelle1992us@yahoo.com>
2021-01-29 16:40:43 -0800

    finsihed form

--- W10D4/todos_p1/frontend/components/todo_list/todo_form.jsx
+++ W10D4/todos_p1/frontend/components/todo_list/todo_form.jsx
@@ -7,6 +7,34 @@ class TodoForm extends React.Component {
             id: Math.floor(Math.random() * 1000000),
             title: ''
         }
+        this.updateTitle = this.updateTitle.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    updateTitle(e) {
+        this.setState({title: e.target.value})
+    }
+
+    handleSubmit(e) {
+        e.preventDefault()
+        this.props.receiveTodo(this.state)
+        this.state = {
+            id: Math.floor(Math.random() * 1000000),
+            title: ''
+        }
+
+    }
+
+    render () {
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <h1>Add Todo</h1>
+                <label>Title: 
+                    <input onChange={this.updateTitle} type="text" value={this.state.title}/>
+                </label>
+                <input type="submit" value="Add Todo"/>
+            </form>
+        )
     }
 
 }
--- W10D4/todos_p1/frontend/components/todo_list/todo_list.jsx
+++ W10D4/todos_p1/frontend/components/todo_list/todo_list.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import TodoForm from './todo_form'
 
 const TodoList = (props) => {
     return (
@@ -9,6 +10,7 @@ const TodoList = (props) => {
                     return <li key={todo.id}>Title: {todo.title}</li>
                 })}
             </ul>
+            <TodoForm receiveTodo={props.receiveTodo}/> 
         </div>
     )
 } 
