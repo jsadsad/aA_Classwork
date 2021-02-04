76dd2c76205c22c2b604cadf741abbe55e52e2de
JCS <joshsad2@gmail.com>
2021-02-04 12:53:26 -0800

    finished setting up skeleton

---
+++ W11D4/benchBnB/frontend/benchbnb.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import createStore from './store/store';
+// import Root from './components/root';
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const root = document.getElementById('root');
+//     let preloadedState = undefined; //makes sure we have a variable we can use.
+//     if (window.currentUser) {
+//         preloadedState = {
+//             session: {
+//                 currentUser: window.currentUser
+//             }
+//         };
+//     }
+//     const store = createStore(preloadedState);
+//     // const store = createStore();
+
+//     ReactDOM.render(<Root store={store} />, root);
+// })
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root");
+    ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+});
