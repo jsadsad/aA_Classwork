6f4af358a0d4b6403588c2be42e9bf1ae0ef44fd
shinara03 <shinara03@gmail.com>
2021-02-04 15:41:55 -0800

    work on session erros reducer

---
+++ W11D4/benchBnB/frontend/reducers/session_errors_reducers.js
@@ -0,0 +1,13 @@
+import {RECEIVE_ERROR, RECEIVE_CURRENT_USER} from '../actions/session_actions'
+
+export default sessionErrorsReducer = (state={}, action) => {
+  Object.freeze(state);
+  switch(action.type) {
+    case RECEIVE_ERROR: 
+      return Object.assign({}, {errors: action.errors})
+    case RECEIVE_CURRENT_USER:
+      return {};
+    default:
+      return state;
+  }
+}
